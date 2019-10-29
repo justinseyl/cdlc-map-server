7e77bd424670eb45440dcb02df1731fe98db8d17
Nick <nickhowell6425@gmail.com>
2019-10-28 23:17:58 -0400

    home_map

---
+++ public/js/map.js
@@ -0,0 +1,17 @@
+$("path, circle").hover(function(e) {
+  $('#info-box').css('display','block');
+  $('#info-box').html($(this).data('info'));
+});
+
+$("path, circle").mouseleave(function(e) {
+  $('#info-box').css('display','none');
+});
+
+var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+if(ios) {
+  $('a').on('click touchend', function() {
+    var link = $(this).attr('href');
+    window.open(link,'_blank');
+    return false;
+  });
+}
