af8a803e148c573617aabc44de438ff5dae9d156
Nick Howell <n.howell@pinnacledesignlab.com>
2020-01-19 23:31:51 -0500

    ad

--- app/routes.js
+++ app/routes.js
@@ -461,7 +461,7 @@ module.exports = function(app, passport) {
 						'processor': 'processornoti.ejs'
 				}
 
-				let query = "select title, description, Concat(DATEDIFF(Now(),created_at),' Days Ago') as time,case when created_at >= date_sub(Now(), interval 1 day) then 'new' end as isnew from ealerts order by created_at desc";
+				let query = "select title, description, Concat(DATEDIFF(Now(),created_at),' Days Ago') as time,Concat('+',DATEDIFF(Now(),created_at)) as mobiletime,case when created_at >= date_sub(Now(), interval 1 day) then 'new' end as isnew from ealerts order by created_at desc";
 				db.query(query, (err, result) => {
 						if (err) throw err;
 
